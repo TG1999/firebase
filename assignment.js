6ceeeb9feb2b7420230e0d19753a626f9a43f9cb
tushar goel <tushar.goel.dav@gmail.com>
2018-08-04 20:12:30 +0530

    first
    Signed-off-by: tushar goel <tushar.goel.dav@gmail.com>

---
+++ assignment.js
@@ -0,0 +1,37 @@
+var firebase=require('firebase-admin');
+const express=require('express')
+const app=express();
+var spawn=require('child_process').spawn;
+const {url}=require('./exports')
+const {pass}=require('./exports')
+firebase.initializeApp({
+    databaseURL:url,
+    credential:firebase.credential.cert('set-my-test-cc6c9-9f3daa9c7b18.json')
+})
+
+app.set('view engine','hbs');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+app.post('/info',(req,res)=>{
+    username=req.body.username;
+    userid=req.body.userid;
+    firebase.database().ref('/users').push({
+        username:username,
+        userid:userid
+    })
+    firebase.database().ref('/userInformation').push({
+        username:username,
+        userid:userid
+    })
+    var proc=spawn('python',['./assignment.py',username,userid,pass]);
+    res.redirect('/')
+})
+
+app.listen(3000,()=>{
+    console.log('listening at http://localhost:3000')
+})
---
+++ assignment.py
@@ -0,0 +1,27 @@
+import smtplib
+import sys
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+userid=sys.argv[2]
+username=sys.argv[1]
+fromaddr = "tushar.goel.dav@gmail.com"
+msg = MIMEMultipart() 
+msg['From'] = fromaddr 
+msg['To'] = userid
+msg['Subject'] = "SET MY TEST"
+body = 'Dear ' +username+ ' Welcome to our app'
+    
+msg.attach(MIMEText(body, 'plain'))
+    
+s = smtplib.SMTP('smtp.gmail.com', 587)
+    
+s.starttls()
+password=sys.argv[3]
+
+s.login(fromaddr,password)
+    
+text = msg.as_string()
+    
+s.sendmail(fromaddr,userid, text)
+    
+s.quit()
