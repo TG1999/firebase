fb0ed83090e7879bae272259518d9cd8121caf74
tusharGoel99 <34160672+tusharGoel99@users.noreply.github.com>
2018-08-11 22:53:46 +0530

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,11 +1,9 @@
 var firebase=require('firebase-admin');
 const express=require('express')
 const app=express();
-var spawn=require('child_process').spawn;
 const {url}=require('./exports')
 const {pass}=require('./exports')
 const {id}=require('./exports')
-var port = process.env.PORT || 3000
 firebase.initializeApp({
     databaseURL:url,
     credential:firebase.credential.cert('set-my-test-cc6c9-9f3daa9c7b18.json')
@@ -26,14 +24,38 @@ app.post('/info',(req,res)=>{
         username:username,
         userid:userid
     })
+    console.log('inserted sucessfully')
     firebase.database().ref('/userInformation').push({
         username:username,
         userid:userid
     })
-    var proc=spawn('python',['./assignment.py',username,userid,pass,id]);
+    var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: id,
+    pass: pass
+  }
+});
+
+var mailOptions = {
+  from:id,
+  to:userid,
+  subject: 'SET MY TEST',
+  text: 'Dear '+username+',Welcome to our app'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
     res.redirect('/')
 })
 
-app.listen(port,()=>{
+app.listen(3000,()=>{
     console.log('listening at http://localhost:3000')
 })
